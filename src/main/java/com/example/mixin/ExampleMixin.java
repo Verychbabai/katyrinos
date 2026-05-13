0f37710f68e669bf53de60fc9e02cbd3b0bf1f00
Verychbabai <145598120+Verychbabai@users.noreply.github.com>
2026-05-13 22:43:59 +0200

    Delete src/main/java/com/example/mixin/ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++
@@ -1,9 +0,0 @@
-@Mixin(Entity.class)
-public abstract class RotationMixin {
-    @Inject(method = "getHeadYaw", at = @At("HEAD"), cancellable = true)
-    private void onGetHeadYaw(CallbackInfoReturnable<Float> info) {
-        if (Killaura.enabled && (Object)this == MinecraftClient.getInstance().player) {
-            // Тут можно подменить Yaw для отрисовки модели от 3 лица
-        }
-    }
-}
